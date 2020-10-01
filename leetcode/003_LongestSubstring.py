feae026d96223a53d9cb03771c374a96c8fc7172
dionysus <dionysus.cho@gmail.com>
2020-10-01 19:38:36 +0000

    [003] accepted

--- leetcode/003_LongestSubstring.py
+++ leetcode/003_LongestSubstring.py
@@ -42,3 +42,56 @@ create a tree?
 min()
 
 '''
+def lengthOfLongestSubstring(s: str) -> int:
+
+    if s == "":
+        return 0
+
+    if len(s) == 1:
+        return 1
+
+    max = 0
+
+    # going backwards from the last letter
+    for i in range(len(s) - 1, -1, -1):
+        
+        # first iteration
+        if i == len(s) - 1:
+            prev = (i, i)
+
+        else:
+            curr = (i, i)
+            # find first match
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j]:
+                    break
+                else:
+                    curr = (i, j)
+                if j == prev[1]:
+                    break
+
+            if curr[1] - curr[0] + 1 > max:
+                max = curr[1] - curr[0] + 1
+
+            prev = curr
+
+    return max          
+
+
+if __name__ == '__main__':
+    s = ("", 0)
+    val = lengthOfLongestSubstring(s[0])
+    s = (" ", 1)
+    val = lengthOfLongestSubstring(s[0])
+    s = ("abcabcbb", 3)
+    val = lengthOfLongestSubstring(s[0])
+    print("{}: {} (expected {})".format(s[0], val, s[1]))
+    s = ("bbbbb", 1)
+    val = lengthOfLongestSubstring(s[0])
+    print("{}: {} (expected {})".format(s[0], val, s[1]))
+    s = ("pwwkew", 3)
+    val = lengthOfLongestSubstring(s[0])
+    print("{}: {} (expected {})".format(s[0], val, s[1]))
+    s = ("cdd", 2)
+    val = lengthOfLongestSubstring(s[0])
+    print("{}: {} (expected {})".format(s[0], val, s[1]))
